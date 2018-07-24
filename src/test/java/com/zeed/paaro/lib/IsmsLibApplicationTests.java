d714694ba8812afea8f1ab6cab80d98e3fa5b734
zeed <yusufsaheedtaiwo@yahoo.com>
2018-07-24 08:30:36 +0100

    added sendgrid dependency. Implementation to be done soon

--- src/test/java/com/zeed/paaro/lib/IsmsLibApplicationTests.java
+++ src/test/java/com/zeed/paaro/lib/IsmsLibApplicationTests.java
@@ -1,17 +1,19 @@
 package com.zeed.paaro.lib;
 
+import com.zeed.paaro.lib.email.sendgrid.Example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class IsmsLibApplicationTests {
 
 	@Test
-	public void contextLoads() {
-
+	public void contextLoads() throws IOException {
+		Example.baselineExample();
 	}
 
 }
